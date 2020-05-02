204b04b317d5fe70141ed270a75d15068d783cf2
bharatrathore <bsrathore0099@gmail.com>
2020-05-02 18:21:51 +0530

    created api endpoints to get the badges data (/api/:id)

--- index.js
+++ index.js
@@ -1,61 +1,21 @@
-const rp = require('request-promise');
-const hid=document.getElementById('hid')
-const getData=document.getElementById('submit')
-const id=""
-getData.addEventListener('click',e=>{
-    
+
+const express=require('express')
+const app=express()
+
+
+const dummy=require('./routes/getbadgeinfo')
+
+app.use('/api',dummy)
+
+app.use((req,res)=>{
+    res.send({
+        message:"You have entered invalid url please enter hostname/api/hackerrankid"
+    })
 })
-const url = 'https://www.hackerrank.com/bharatrathore13';
-const $ = require('cheerio');
-let userInfo={}
-let badgeInfo={}
 
-let returned=rp(url)
-  .then(function(html){
-    //success!
-     let name=$('h1.profile-heading',html).text();
-     
-    const badgeType=[]
-    const stars=[]
-    
-    $('.badges-list > .hacker-badge .badge-title',html).each(function(){
-       let bt=$(this).text();
-       if (bt.toLowerCase()=="days of code"){
-           bt='30 '+bt
-       }
-       if (bt.toLowerCase()=="days of js"){
-        bt='10 '+bt
-    }
-       
-        badgeType.push(bt)
-    })
-    $('.star-section',html).each(function(i,e){
-        //console.log("----------------------------------------")
-
-        stars.push(e.children[0].children.length)
-    })
-    return [name,badgeType,stars]
-  })
-  .catch(function(err){
-
-
-
-    //handle error
-  });
-
-returned.then(body=>{
-    let badges={}
-    body[1].forEach((e,i)=>{
-        badges[e.toString()]=body[2][i]
-    })
-    //console.log(badges)
-let mainInfo={}
-let sum=0;
-mainInfo.name=body[0]
-mainInfo.badges=badges
-body[2].forEach(e=>{sum+=e})
-mainInfo.totalBadges=body[1].length
-mainInfo.totalStars=sum
-console.log(mainInfo)
+app.listen(3000,()=>{
+    console.log("Running on port 3000")
 })
+
+
 //console.log(name)
---
+++ routes/getbadgeinfo.js
@@ -0,0 +1,63 @@
+const express=require('express')
+
+const router=express.Router()
+
+//Web scraping data
+const rp = require('request-promise');
+let url = 'https://www.hackerrank.com/';
+const $ = require('cheerio');
+
+router.get('/:id',(req,res)=>{
+    id=req.params.id    
+    url=url+id
+        let returned=rp(url)
+    .then(function(html){
+        //success!
+        let name=$('h1.profile-heading',html).text();
+        
+        const badgeType=[]
+        const stars=[]
+        
+        $('.badges-list > .hacker-badge .badge-title',html).each(function(){
+        let bt=$(this).text();
+        if (bt.toLowerCase()=="days of code"){
+            bt='30 '+bt
+        }
+        if (bt.toLowerCase()=="days of js"){
+            bt='10 '+bt
+        }
+        
+            badgeType.push(bt)
+        })
+        $('.star-section',html).each(function(i,e){
+            //console.log("----------------------------------------")
+
+            stars.push(e.children[0].children.length)
+        })
+        return [name,badgeType,stars]
+    })
+    .catch(function(err){
+
+        
+
+        //handle error
+    });
+
+    returned.then(body=>{
+        let badges={}
+        body[1].forEach((e,i)=>{
+            badges[e.toString()]=body[2][i]
+        })
+        //console.log(badges)
+    let mainInfo={}
+    let sum=0;
+    mainInfo.name=body[0]
+    mainInfo.badges=badges
+    body[2].forEach(e=>{sum+=e})
+    mainInfo.totalBadges=body[1].length
+    mainInfo.totalStars=sum
+    res.send(mainInfo)
+    })
+})
+
+module.exports=router
