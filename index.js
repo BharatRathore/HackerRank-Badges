c1c442772d5e01e08493ea7ccf6af4500aecb946
bharatrathore <bsrathore0099@gmail.com>
2020-05-02 18:46:41 +0530

    configured port for prod deploy, created procfile for heroku

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 
 const express=require('express')
 const app=express()
-
+let port=process.env.port||3000
 
 const dummy=require('./routes/getbadgeinfo')
 
 app.use('/api',dummy)
 
 app.use((req,res)=>{
-    res.send({
+    res.json({
         message:"You have entered invalid url please enter hostname/api/hackerrankid"
     })
 })
 
-app.listen(3000,()=>{
+app.listen(port,()=>{
     console.log("Running on port 3000")
 })
 
