51704d78c2b7b4b180b8ef5792bb847ebdf13698
bharatrathore <bsrathore0099@gmail.com>
2020-05-12 00:07:37 +0530

    fixed wrong data bug when type was 10 days of statistics

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 3001;
 
 const dummy = require("./routes/getbadgeinfo");
 
--- routes/getbadgeinfo.js
+++ routes/getbadgeinfo.js
@@ -8,60 +8,64 @@ let hitCount=0
 router.get("/:id", (req, res) => {
   id = req.params.id;
   url = url + id;
+  
+ rp(url).then(function (html) {return html}).then((html)=>{
+  let name = $("h1.profile-heading", html).text();
+  console.log(name)
+  const badgeType = [];
+  const stars = [];
+  if (name!="")  {
+    $(".badges-list > .hacker-badge .badge-title", html).each(function () {
+        let bt = $(this).text();
+        if (bt.toLowerCase() == "days of code") {
+          bt = "30 " + bt;
+        }
+        if (bt.toLowerCase() == "days of js") {
+          bt = "10 " + bt;
+        }
+        if (bt.toLowerCase() !="days of") {
+          badgeType.push(bt);
+        }
 
- rp(url).then(function (html) {
-      
-      let name = $("h1.profile-heading", html).text();
-      const badgeType = [];
-      const stars = [];
-      if (name!="")  {
-        $(".badges-list > .hacker-badge .badge-title", html).each(function () {
-            let bt = $(this).text();
-            if (bt.toLowerCase() == "days of code") {
-              bt = "30 " + bt;
-            }
-            if (bt.toLowerCase() == "days of js") {
-              bt = "10 " + bt;
-            }
-    
-            badgeType.push(bt);
-          });
-          let sum = 0;
-          $(".star-section", html).each(function (i, e) {
-            
-            star=e.children[0].children.length
-            sum+=star
-            stars.push(star);
-          });
-            let badges = {};
-            badgeType.forEach((e, i) => {
-            badges[e.toString()] = stars[i];
-            });
-            
-            let mainInfo = {};
-            
-            mainInfo.status="OK"
-            mainInfo.name = name;
-            mainInfo.badges = badges;
-            // stars.forEach((e) => {
-            // sum += e;
-            // });
-            mainInfo.totalBadges = badgeType.length;
-            mainInfo.totalStars = sum;
-            res.send(mainInfo);
-            
-      }
-      else{
-          res.send({status:"ERROR",message:"No Such User Exists"})
-      }
-      hitCount++
-      console.log(hitCount)
-      url = "https://www.hackerrank.com/";
-       // return [name, badgeType, stars];
-    })
-    .catch(function (err) {
-      console.log(err);
-    });
+        
+      });
+      let sum = 0;
+      $(".star-section", html).each(function (i, e) {
+        
+        star=e.children[0].children.length
+        sum+=star
+        stars.push(star);
+      });
+        let badges = {};
+        badgeType.forEach((e, i) => {
+        badges[e.toString()] = stars[i];
+        });
+        
+        let mainInfo = {};
+        
+        mainInfo.status="OK"
+        mainInfo.name = name;
+        mainInfo.badges = badges;
+        mainInfo.totalBadges = badgeType.length;
+        mainInfo.totalStars = sum;
+        res.send(mainInfo);
+        
+  }
+  else{
+    res.send({status:"ERROR",message:"No Such User Exists"})
+  }
+  hitCount++
+  console.log(hitCount)
+  url = "https://www.hackerrank.com/";
+   // return [name, badgeType, stars];
+   
+ }).catch(function (err) {
+  console.log(err);
 });
+      
+      
+    })
+    
+
 
 module.exports = router;
